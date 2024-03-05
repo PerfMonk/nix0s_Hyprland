9d093ce332a742ea6cc9f2828d51278047e34608
Bernard Tremblay <tremblay.bernard@gmail.com>
2024-03-05 17:59:54 -0500

    Delete dotfiles directory

--- dotfiles/scripts/LockScreen.sh
+++
@@ -1,5 +0,0 @@
-#!/run/current-system/sw/bin/bash
-
-CONFIG="$HOME/.config/swaylock/config"
-
-sleep 0.5s; swaylock --config ${CONFIG} & disown
--- dotfiles/scripts/Sunset.sh
+++
@@ -1,2 +0,0 @@
-#!/run/current-system/sw/bin/bash
-wlsunset -t 4000 -T 6500 -d 900 -S 07:00 -s 19:00
